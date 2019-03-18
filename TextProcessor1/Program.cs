18e34686c88ae55c375230426135ef42ed01b434
Alex <mindwind@list.ru>
2019-03-18 23:47:27 +0500

    Добавлено чтение из БД

--- TextProcessor1/Program.cs
+++ TextProcessor1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -37,8 +38,9 @@ namespace TextProcessor1
             }
             else if(args.Length==0)
             {
-                Console.WriteLine("Введена пустая команда");
-                Console.ReadLine();
+                WordProcessing();
+                //Console.WriteLine("Введена пустая команда");
+                //Console.ReadLine();
             }
             else
             {
@@ -52,6 +54,7 @@ namespace TextProcessor1
             {
                 string path = @"C:\Users\mindw\source\repos\TextProcessor1\TextProcessor1\Text.txt";
                 string text;
+                int count = 1;
                 string[] separators = { ",", ".", "!", "?", ";", ":", " " };
                 try
                 {
@@ -62,10 +65,24 @@ namespace TextProcessor1
                     }
                     string[] words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
                     Array.Sort(words);
-                    foreach (var word in words)
+                    //foreach (var word in words)
+                    //{
+                    //    Console.WriteLine(word);
+                    //}
+                    
+                    for (int i = 0; i < words.Length-1; i++)
                     {
-                        Console.WriteLine(word);
+                        if (words[i]==words[i+1])
+                        {
+                            count++;
+                        }
+                        else
+                        {
+                            if (count>=3 && words[i].Length>=3) db.Frequencies.Add(new Frequency { Word = words[i], Amount = count });
+                            count = 1;
+                        }
                     }
+                    db.SaveChanges();
                     Console.WriteLine(words.Length);
 
                     //Console.WriteLine(text);
@@ -74,8 +91,8 @@ namespace TextProcessor1
                 {
                     Console.WriteLine(e.Message);
                 }
-                db.Frequencies.Add(new Frequency { Word = "Овощи", Amount = 37 });
-                db.SaveChanges();
+                //db.Frequencies.Add(new Frequency { Word = "Овощи", Amount = 37 });
+                //db.SaveChanges();
                 //Frequency frequency1 = new Frequency { Word = "Арбуз", Amount = 7 };
                 //Frequency frequency2 = new Frequency { Word = "Автобус", Amount = 2 };
                 //db.Frequencies.Add(frequency1);
@@ -83,13 +100,13 @@ namespace TextProcessor1
                 //db.SaveChanges();
                 //Console.WriteLine("Объекты успешно сохранены");
 
-                var frequencies = db.Frequencies;
-                Console.WriteLine("Список объектов:");
-                foreach (Frequency f in frequencies)
-                {
-                    Console.WriteLine("{0} - {1}", f.Word, f.Amount);
-                }
-                Console.ReadLine();
+                //var frequencies = db.Frequencies;
+                //Console.WriteLine("Список объектов:");
+                //foreach (Frequency f in frequencies)
+                //{
+                //    Console.WriteLine("{0} - {1}", f.Word, f.Amount);
+                //}
+                //Console.ReadLine();
             }
         }
         static void UpdateDictionary()
@@ -98,11 +115,33 @@ namespace TextProcessor1
         }
         static void ClearDictionary()
         {
-
+            using (FrequencyContext db = new FrequencyContext())
+            {
+                db.Database.SqlQuery<Frequency>("TRUNCATE TABLE [Frequencies]");
+            }
         }
         static void WordProcessing()
         {
-            
+            string inpWord,sqlQuery1;
+            ConsoleKeyInfo cki = new ConsoleKeyInfo();
+            while (true)
+            {
+                //cki = Console.ReadKey(true);
+                inpWord = Console.ReadLine();
+                
+                if (inpWord == "") break;
+                
+                System.Data.SqlClient.SqlParameter param = new System.Data.SqlClient.SqlParameter("@word", inpWord+"%");
+                sqlQuery1 = "select top 5 * from Frequencies where Word like @word order by Amount desc, Word desc";
+                using (FrequencyContext db = new FrequencyContext())
+                {
+                    var frequency1 = db.Database.SqlQuery<Frequency>(sqlQuery1,param);
+                    foreach (var item in frequency1)
+                    {
+                        Console.WriteLine(item.Word);
+                    }
+                }
+            }
         }
     }
 }
